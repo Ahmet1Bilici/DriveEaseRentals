5411939ce923e604030a9c9e043a4de450e132cb
Ahmet <1yigitbilici@gmail.com>
2023-09-23 17:20:44 -0500

    Front end additions

---
+++ types/index.ts
@@ -0,0 +1,13 @@
+import React, { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
+}
+
+export interface SearchManufacturerProps {
+  manufacturer: String;
+  setManufacturer: (manufacturer: string) => void;
+}
