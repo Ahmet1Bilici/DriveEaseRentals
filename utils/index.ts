9418cf2699108ea510ffcf1f34b40ff5b7a9e572
Ahmet <1yigitbilici@gmail.com>
2023-09-26 12:36:24 -0500

    Bug Fixes

--- utils/index.ts
+++ utils/index.ts
@@ -4,13 +4,15 @@ import dotenv from "dotenv";
 export async function fetchCars(filters: FilterProps) {
   const { manufacturer, year, model, limit, fuel } = filters;
   const headers = {
-    "X-RapidAPI-Key": process.env.X_RapidAPI_Key,
+    "X-RapidAPI-Key": "6611b2db01msh8dbbbc1e4233aa1p1a40e5jsn08c84193580c",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
   const response = await fetch(
     `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
-    { headers: headers }
+    {
+      headers: headers,
+    }
   );
 
   const result = await response.json();
